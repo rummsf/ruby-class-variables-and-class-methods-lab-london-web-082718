c6e6a30c83954819a5f33a9e1201bec2d699e6e3
Irum F <irummf@gmail.com>
2018-09-03 08:06:00 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song 
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@genre_count = {}
+    @@artist_count = {}
+    @@count += 1 
+    @@genres << @genre 
+    @@artists << @artist 
+  end
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genre_count(genre_name, number)
+    @@genres.each do |genre|
+  end
+  
+  def self.artist 
+    @@artists
+  end 
+  
+  def self.artist_count
+    @@artists << @artist if !@@artists.include?(@artist)
+  end
+end 
+end 
